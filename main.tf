284e3609274078757643b4cfbf8a281b2c3afc6e
patryKKRK <patryk.orlikowski@gmail.com>
2020-11-17 14:10:39 +0100

    imported the networking module and created .gitignore

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket  = "patrykorlikowski1"
+    key     = "chaos1.tfstate"
+    region  = "eu-central-1"
+    profile = "chaos-playground"
+  }
+}
+provider "aws" {
+  profile = "chaos"
+  region  = "eu-central-1"
+}
+
+module "networking" {
+  source = "github.com/patryKKRK/chaos1"
+}
